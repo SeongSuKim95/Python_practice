cf4d54917d6bb50f1e72b7fb417c937efacba2e3
SeongSu Kim <pand_a@naver.com>
2022-06-09 17:50:36 +0900

    Dijkstra, string_compression

--- Dynamic_Programming.py
+++ Dynamic_Programming.py
@@ -96,3 +96,24 @@ if d[m] == 10001:
     print(-1)
 else :
     print(d[m])
+
+
+# Longest Increasing Subsequence (가장 긴 증가하는 부분 수열)
+# D[i] = array[i]를 마지막 원소로 가지는 부분 수열의 최대 길이
+# 모든 0 <= j < i에 대하여, D[i] = max(D[i],D[j]+1) if array[j] < array[i]
+# 시간 복잡도 O(n^2)
+
+n = int(input())
+
+array = list(map(int,input().split()))
+array.reverse()
+
+dp = [1] * n
+
+for i in range(1,n):
+    for j in range(0,i):
+        if array[j] < array[i]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(n-max(dp))
+
